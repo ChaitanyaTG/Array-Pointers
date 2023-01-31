bbdf6a57f8c3941d3da82a47a5d0a915379e4cf3
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-02-01 02:28:50 +0530

    Add files via upload

---
+++ DrawingPattern3.cpp
@@ -0,0 +1,25 @@
+//
+//  DrawingPattern3.cpp
+//  Arrays
+//
+//  Created by Chaitanya Gadkari on 29/01/23.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (i > j) {
+                cout<<" ";
+            }
+            else {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
