35fa3f9b94369967f25625b18b258f9e573c51a6
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-02-02 02:30:13 +0530

    Add files via upload

---
+++ DrawingPattern/UpperTriangleOf*.cpp
@@ -0,0 +1,26 @@
+//
+//  UpperTriangleOf*.cpp
+//  Arrays
+//
+//  Created by Chaitanya Gadkari on 29/01/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if ( i <= j) {
+                cout<<"* ";
+            }
+            else {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/
