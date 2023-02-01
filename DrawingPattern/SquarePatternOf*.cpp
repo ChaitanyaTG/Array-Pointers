1ad00a3a8905de524d7ebea9fd66ac7945281abf
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-02-02 02:30:29 +0530

    Add files via upload

---
+++ DrawingPattern/SquarePatternOf*.cpp
@@ -0,0 +1,20 @@
+//
+//  SquarePatternOf*.cpp
+//  Arrays
+//
+//  Created by Chaitanya Gadkari on 29/01/23.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
