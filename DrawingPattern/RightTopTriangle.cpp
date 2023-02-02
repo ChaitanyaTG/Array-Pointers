82dcbfd27bc6941babcfed86eaf98b250a8d3e24
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-02-03 03:11:45 +0530

    Add files via upload

---
+++ DrawingPattern/RightTopTriangle.cpp
@@ -0,0 +1,25 @@
+//
+//  RightTopTriangle.cpp
+//  Arrays
+//
+//  Created by Chaitanya Gadkari on 29/01/23.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if (i <= j) {
+                cout<<"* ";
+            }
+            else {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
